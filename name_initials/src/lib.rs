9ba55697c879eda1cfd53e22e02824e24cfa210f
serignmbaye <barsaliou1998@gmail.com>
2024-04-22 16:47:16 +0000

    fix: update name initials sub

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -4,11 +4,9 @@ pub fn initials(names: Vec<&str>) -> Vec<String> {
         let mut initials_str = String::new();
         let mut words = name.split_whitespace();
         while let Some(word) = words.next() {
-            let mut chars = word.chars();
-            if let Some(initial) = chars.next() {
-                initials_str.push(initial);
-                initials_str.push_str(". ");
-            }
+            let Some(initial) = word.chars().next() else { continue; };
+            initials_str.push(initial);
+            initials_str.push_str(". ");
         }
         initials.push(initials_str.trim().to_string());
     }
