8ccf36938ce6f6ff111085feef36e129d5ede9ed
serignmbaye <barsaliou1998@gmail.com>
2024-04-22 11:10:51 +0000

    feat: looping sub

---
+++ looping/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+
+fn main() {
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+    let answer = "The letter e";
+    let mut tries = 0;
+
+    loop {
+        tries += 1;
+        println!("{}", riddle);
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).unwrap();
+        if user_input.trim() == answer {
+            println!("Number of trials: {}", tries);
+            break;
+        }
+    }
+}
