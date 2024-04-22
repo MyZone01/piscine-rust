5b452746c16cdcdd25bb483c7d450f65c4911691
serignmbaye <barsaliou1998@gmail.com>
2024-04-22 11:16:08 +0000

    feat: groceries sub

---
+++ groceries/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn insert(vec: &mut Vec<String>, item: String) {
+    vec.push(item);
+}
+
+pub fn at_index(vec: &Vec<String>, index: usize) -> String {
+    let item = vec.get(index);
+    match item {
+        Some(value) => value.to_string(),
+        None => String::from("Index out of bounds"),
+    }
+}
