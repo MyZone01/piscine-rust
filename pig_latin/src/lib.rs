10a396bf42f6ad89dc27cc0b84c856842676ecad
serignmbaye <barsaliou1998@gmail.com>
2024-05-03 09:52:22 +0000

    fix: pig latin sub

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -38,7 +38,7 @@
 pub fn pig_latin(text: &str) -> String {
     for (i, c) in text.chars().enumerate() {
         if "aeiou".contains(c) {
-            if c == 'u' && i != 0 && text[i - 1..].starts_with("qu") {
+            if c == 'u' && i >= 2 && text[i - 2..].starts_with("squ") {
                 return text[i + 1..].to_string() + &text[..i + 1] + "ay";
             }
             return text[i..].to_string() + &text[..i] + "ay";
