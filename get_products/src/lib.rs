7d8bde2026584f7e2d7b17f6faf6d3a35ba95825
serignmbaye <barsaliou1998@gmail.com>
2024-05-13 12:11:44 +0000

    feat: add step iterator sub

--- get_products/src/lib.rs
+++ get_products/src/lib.rs
@@ -13,10 +13,8 @@
 
 pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
     if arr.len() <= 1 {
-        return vec![];
+        return Vec::new();
     }
-    arr.iter()
-        .enumerate()
-        .map(|(i, _)| arr.iter().filter(|&x| *x != arr[i]).product())
-        .collect()
+    let prod: usize = arr.iter().product();
+    arr.iter().map(|num| prod / num).collect()
 }
