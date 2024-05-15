060a8ec893dea993fec9e5eeda3161de5835dbdd
serignmbaye <barsaliou1998@gmail.com>
2024-05-15 12:49:38 +0000

    feat: add lifetime sub

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,44 @@
+// Instructions
+// Complete the Person struct with the fields and associated function described below. new should set the age to 0.
+
+// Expected Functions and Data Structures (Both need to be completed)
+// #[derive(Debug)]
+// pub struct Person{
+// 	pub name: &str,
+// 	pub age: u8,
+// }
+
+// impl Person {
+// 	pub fn new(name: &str) -> Person {
+// 	}
+// }
+// Usage
+// Here is a program to test your function.
+
+// use lifetimes::*;
+
+// fn main() {
+// 	let person = Person::new("Leo");
+
+// 	println!("Person = {:?}", person);
+// }
+// And its output:
+
+// $ cargo run
+// Person = Person { name: "Leo", age: 0 }
+// $
+
+#[derive(Debug, Clone)]
+pub struct Person<'a> {
+    pub name: &'a str,
+    pub age: u8,
+}
+
+impl<'a> Person<'a> {
+    pub fn new(name: &'a str) -> Person<'a> {
+        Person {
+            name,
+            age: 0,
+        }
+    }
+}
